226d69e4c2eaf19e9acca8447fd97b9db8233aeb
dogukanayd <daydogdu93@gmail.com>
2018-09-24 17:39:40 +0300

    eslint airbnb

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,20 @@
-const passport = require("passport");
-const crypto = require("crypto");
-const mongoose = require("mongoose");
-const User = mongoose.model("User");
-const promisify = require("es6-promisify");
+const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
-exports.login = passport.authenticate("local", {
-  failureRedirect: "/user/login",
-  failureFlash: "Failed Login!",
-  successRedirect: "/user/dashboard",
-  successFlash: "You are now logged in!"
+exports.login = passport.authenticate('local', {
+  failureRedirect: '/user/login',
+  failureFlash: 'Failed Login!',
+  successRedirect: '/user/dashboard',
+  successFlash: 'You are now logged in!'
 });
 
 exports.logout = (req, res) => {
   req.logout();
-  req.flash("success", "You are now logged out!");
-  res.redirect("/");
+  req.flash('success', 'You are now logged out!');
+  res.redirect('/');
 };
 
 exports.isLoggedIn = (req, res, next) => {
@@ -23,6 +23,6 @@ exports.isLoggedIn = (req, res, next) => {
     next();
     return;
   }
-  req.flash("error", "Oops you must be logged in to do that!");
-  res.redirect("login");
+  req.flash('error', 'Oops you must be logged in to do that!');
+  res.redirect('login');
 };
--- start.js
+++ start.js
@@ -1,23 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // import environmental variables from our variables.env file
-require("dotenv").config({ path: "variables.env" });
+require('dotenv').config({ path: 'variables.env' });
 
 // Connect to our Database and handle any bad connections
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connection.on("error", err => {
-    console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+mongoose.connection.on('error', (err) => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
 // READY?! Let's go!
 
-//import all of our models
-require("./models/User");
+// import all of our models
+require('./models/User');
 
 // Start our app!
-const app = require("./app");
-app.set("port", process.env.PORT || 7777);
-const server = app.listen(app.get("port"), () => {
-    console.log(`Express running → PORT ${server.address().port}`);
+const app = require('./app');
+
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
 });
